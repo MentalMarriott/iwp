e929b558bbc36aec7ccbd45698afcee1e27e87ed
Chris <cpm4@aber.ac.uk>
2013-04-18 09:17:21 +0100

    added rotate on click, next get to move to face mouse

--- object.js
+++ object.js
@@ -1,35 +1,85 @@
-var context;
+//var context;
 //var x, y;
+
+function follow(ev)
+{
+//        var x = document.images.player.offsetLeft + 10;
+//	var y = document.images.player.y;
+
+//	player(400, 200, 5, ev); 
+}
+
+
 
 function ev_mousemove(ev) 
 {
+
 	var context = document.getElementById('myCanvas').getContext('2d');
 	var x, y;
 
-/*	if (ev.layerX || ev.layerX === 0) 
-	{ // Firefox
-		x = ev.layerX;
-		y = ev.layerY;
-	}
-	else if (ev.offsetX || ev.offsetX === 0) 
-	{ // Opera
-		x = ev.offsetX;
-		y = ev.offsetY;
-	}*/
 	var pos = mouseLoc(ev);
 	x = pos [0];
 	y = pos [1];
 	
 	console.log(x);
 	
-	context.clearRect(0,0,800,400);
+	context.clearRect(0,0,document.width, document.height);
 
+	//prints x and y co ordinates of mouse
 	document.getElementById("mousePos").innerHTML = ("x=" + x + ", y="  + y);
 
 	y = y - 75;
 	
 	player(x, y);	
 }
+
+function rotate()
+{
+	var player = new Image();
+	player.src = "ball.png";
+
+	var xcent = 400 + player.width/2;
+	var ycent = 200 + player.height/2;
+
+	
+	context.clearRect(0, 0, 800, 400);
+
+	context.save();
+
+	context.translate(xcent, ycent);
+
+	context.rotate(7 * Math.PI / 180);
+
+	context.translate(-xcent, -ycent);
+	//context.clearRect(0, 0, document.width, document.height);
+	context.drawImage(player, xcent - player.width / 2, ycent - player.height / 2);
+
+}
+
+function player(x, y, angle, ev)
+{
+	context.clearRect(0, 0, document.width-200, document.height-200);
+	
+//	var pos = mouseLoc(ev);
+//	x = pos[0]-30;
+//	y = pos[1]-100;	
+
+	var player = new Image();
+	player.src = "ball.png";
+
+//	 context.save();
+//	 context.translate(x,y);     
+//	 context.rotate(angle * Math.PI / 180);
+         context.drawImage(player, x, y); 
+//         context.restore();  
+
+
+//	context.drawImage(player, 0, 0);
+
+	 // And restore the context ready for the next loop
+    //context.restore();
+}
+
 
 /**
  *Returns mouse current x and y location
@@ -51,27 +101,25 @@ var mouseLoc = function(ev)
 	}
 
 	return [x, y];
-};
+}
 
-function player(x, y)
+function bullet(x, y)
 {
-	console.log(x);
-	console.log(y);
-	context.beginPath();
-	context.beginPath();	context.fillStyle="#0000ff";
-	context.arc(x, y, 6, 0, Math.PI*2, true);
-	context.closePath();
-	context.fill();
-	context.shadowColor = "#000";
-	context.shadowBlur = 20;
-	context.shadowOffsetX = 10;
-	context.shadowOffsetY = 10;
+	var players = new Array();
+
+	base_image = new Image();
+	base_image.src = 'ball.png';
+
+	players.push(base_image);
+
+	
 }
 
 
 //player fire
 function fire(ev)
 {
+	//gets and stores mouses x and y co ordinates
 	var x, y;
 	var pos = mouseLoc(ev)
 	x = pos[0];
@@ -90,15 +138,16 @@ function fire(ev)
 	function frame() 
 	{
 	
-		context.clearRect(0, 0, 800, 400);
+		context.clearRect(0, 0, document.width, document.height);
 
-		player(x, y);
+		//left = left + 1;
 
-		x = x + 1 // show frame 
+		bullet(x, y);
 
-		draw_player(x, y);
+		x = x + 1  // moves img 1 along x axis 
 
-		if (left == 100)  // check finish condition
+
+		if (x > 800)  // check finish condition
 		clearInterval(id)
 	}
 	
@@ -106,48 +155,3 @@ function fire(ev)
 	
 }
 
-function retrun_bullets()
-{
-	var bullets = new Array();
-
-	return bullets
-}
-
-function update()
-{
-	travel
-}
-
-/*
-function travel(x, y, bullets)
-{
-	var x, y;
-	
-	context.clearRect(0, 0, 800, 400);
-
-	for bullet in bullets
-	{
-		bullet.x += 10;
-		draw_bullets(x, y);
-	}
-}
-
-
-/**
- *Draws bullet at location of mouse
- */
-function draw_bullet(x, y)
-{
-	context.beginPath();
-	context.beginPath();
-	context.fillStyle="#0000ff";
-	context.arc(x, y, 6, 0, Math.PI*2, true);
-	context.closePath();
-	context.fill();
-	context.shadowColor = "#000";
-	context.shadowBlur = 20;
-	context.shadowOffsetX = 10;
-	context.shadowOffsetY = 10;
-
-	
-}
